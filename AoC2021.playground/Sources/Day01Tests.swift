6fb562d557b255587025bdbb07a1d96ce4871d48
Jake Turner <jake@evansturner.co.uk>
2021-12-01 19:19:20 +0000

    updated gitignbore

--- AoC2021.playground/Sources/Day01Tests.swift
+++ AoC2021.playground/Sources/Day01Tests.swift
@@ -1,8 +1,22 @@
-//
-//  Day01Tests.swift
-//  AoC2021
-//
-//  Created by Jake on 01/12/2021.
-//
+import XCTest
 
-import Foundation
+class Day01Tests : XCTestCase
+{
+  func Part1()
+  {
+    let vals: [Int] = [
+ 199,
+ 200,
+ 208,
+ 210,
+ 200,
+ 207,
+ 240,
+ 269,
+ 260,
+ 263]
+    let expected = 3
+    let result = Day01.CountWindow(values: vals, window: 1)
+    XCTAssertEqual(expected, result)
+  }
+}
