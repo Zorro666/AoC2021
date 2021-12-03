1ab28f1b0b6bcf6669e4fc828c30b36707a4e39f
Jake Turner <jake@evansturner.co.uk>
2021-12-03 06:22:58 +0000

    Day03 Completed

--- AoC2021SwiftTests/Day03Tests.swift
+++ AoC2021SwiftTests/Day03Tests.swift
@@ -3,7 +3,43 @@ import XCTest
 
 class Day03Tests: XCTestCase {
 
-    func testExample() throws {
-      XCTAssert(false)
+    func testPower() throws {
+      let testInput = [
+"00100",
+"11110",
+"10110",
+"10111",
+"10101",
+"01111",
+"00111",
+"11100",
+"10000",
+"11001",
+"00010",
+"01010"
+      ]
+      let expected = 198
+      let actual = Day03.Power(lines:testInput)
+      XCTAssertEqual(expected, actual)
+    }
+
+    func testLifeSupport() throws {
+      let testInput = [
+"00100",
+"11110",
+"10110",
+"10111",
+"10101",
+"01111",
+"00111",
+"11100",
+"10000",
+"11001",
+"00010",
+"01010"
+      ]
+      let expected = 230
+      let actual = Day03.LifeSupport(lines:testInput)
+      XCTAssertEqual(expected, actual)
     }
 }
