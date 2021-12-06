c539f6613174cc6c0ef8a66eab4ecc9ae36bd0ff
Jake Turner <jake@evansturner.co.uk>
2021-12-06 07:32:14 +0000

    Day06 Done

--- AoC2021SwiftTests/Day06Tests.swift
+++ AoC2021SwiftTests/Day06Tests.swift
@@ -4,18 +4,29 @@ import XCTest
 class Day06Tests: XCTestCase {
   
   static let input = """
+3,4,3,1,2
 """
-  func testPart1() throws {
+  func testPart1_18_26() throws {
     let lines = Day06Tests.input.components(separatedBy: ["\n"])
-    let expected = 5
-    let actual = Day06.Part1(lines: lines)
+    let expected = 26
+    Day06.Parse(lines: lines)
+    let actual = Day06.Simulate(days: 18)
+    XCTAssertEqual(expected, actual)
+  }
+  
+  func testPart1_80_5934() throws {
+    let lines = Day06Tests.input.components(separatedBy: ["\n"])
+    let expected = 5934
+    Day06.Parse(lines: lines)
+    let actual = Day06.Simulate(days: 80)
     XCTAssertEqual(expected, actual)
   }
   
   func testPart2() throws {
     let lines = Day06Tests.input.components(separatedBy: ["\n"])
-    let expected = 12
-    let actual = Day06.Part2(lines: lines)
+    let expected = 26984457539
+    Day06.Parse(lines: lines)
+    let actual = Day06.Simulate(days: 256)
     XCTAssertEqual(expected, actual)
   }
 }
